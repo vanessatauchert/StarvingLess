43a33aaa7154f54e25e460f73be0e5b84e2fafff
Vanessa <tauchertvanessa@gmail.com>
2023-03-13 14:12:06 -0300

    add UserRepository

---
+++ src/main/java/com/fatec/starvingless/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.fatec.starvingless.repositories;
+
+import com.fatec.starvingless.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+}
