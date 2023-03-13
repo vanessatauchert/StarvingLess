d7997e8d67789af0150d9c5765fcfcd816c6b142
Vanessa <tauchertvanessa@gmail.com>
2023-03-13 11:04:11 -0300

    first commit

---
+++ src/main/java/com/fatec/starvingless/StarvinglessApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.starvingless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarvinglessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarvinglessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/starvingless/StarvinglessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.starvingless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarvinglessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
