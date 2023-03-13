e64c45e78f4d08e907b82e9963cc26c5186b1933
Vanessa <tauchertvanessa@gmail.com>
2023-03-13 16:22:42 -0300

    add findEmail and CPF

--- src/main/java/com/fatec/starvingless/repositories/UserRepository.java
+++ src/main/java/com/fatec/starvingless/repositories/UserRepository.java
@@ -4,6 +4,10 @@ import com.fatec.starvingless.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByCpf(String cpf);
+    Optional<User> findByEmail(String email);
 }
