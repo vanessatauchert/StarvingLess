5c8fa315f4e0ac4732b0e9b800c3dd31fd1b7311
Vanessa <tauchertvanessa@gmail.com>
2023-03-13 14:08:37 -0300

    add User Class

---
+++ src/main/java/com/fatec/starvingless/entities/User.java
@@ -0,0 +1,32 @@
+package com.fatec.starvingless.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String address;
+    private String password;
+    @Column(unique = true)
+    private String email;
+    private String phone;
+    private String signUpDate;
+
+
+}
