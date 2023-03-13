687095b8a2c1ce77923f1aee2c88036d3910f860
Vanessa <tauchertvanessa@gmail.com>
2023-03-13 16:22:22 -0300

    add create with UserExist

--- src/main/java/com/fatec/starvingless/services/UserService.java
+++ src/main/java/com/fatec/starvingless/services/UserService.java
@@ -1,8 +1,10 @@
 package com.fatec.starvingless.services;
 
+import com.fatec.starvingless.dto.UserDTO;
 import com.fatec.starvingless.entities.User;
 import com.fatec.starvingless.repositories.UserRepository;
 import com.fatec.starvingless.services.exceptions.ObjectNotFoundException;
+import com.fatec.starvingless.services.exceptions.UserAlreadyExistsException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -26,5 +28,16 @@ public class UserService {
 
     public Page<User> findAll(Pageable pageable){
         return repository.findAll(pageable);
+    }
+
+    public User create(UserDTO userDTO){
+        userDTO.setId(null);
+        if (repository.findByCpf(userDTO.getCpf()).isPresent()) {
+            throw new UserAlreadyExistsException("CPF already exists.");
+        }
+        if (repository.findByEmail(userDTO.getEmail()).isPresent()) {
+            throw new UserAlreadyExistsException("Email already exists.");
+        }
+        return repository.save(mapper.map(userDTO, User.class));
     }
 }
