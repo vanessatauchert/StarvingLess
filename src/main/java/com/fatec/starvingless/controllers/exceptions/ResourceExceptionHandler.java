5c5b27aa84c0d753d73f1bd3759907001864b2d8
Vanessa <tauchertvanessa@gmail.com>
2023-03-13 15:16:25 -0300

    add cpf, email and date exceptions

--- src/main/java/com/fatec/starvingless/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/fatec/starvingless/controllers/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.fatec.starvingless.controllers.exceptions;
 
+import com.fatec.starvingless.services.exceptions.InvalidCpfException;
+import com.fatec.starvingless.services.exceptions.InvalidDateException;
+import com.fatec.starvingless.services.exceptions.InvalidPhoneException;
 import com.fatec.starvingless.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,27 @@ public class ResourceExceptionHandler {
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
                 "Object Not Found", ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(InvalidCpfException.class)
+    public ResponseEntity<StandardError> invalidCpfException(InvalidCpfException ex, HttpServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Invalid CPF format", ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(InvalidPhoneException.class)
+    public ResponseEntity<StandardError> invalidPhoneException(InvalidPhoneException ex, HttpServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Invalid phone format", ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(InvalidDateException.class)
+    public ResponseEntity<StandardError> invalidDateException(InvalidDateException ex, HttpServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Invalid date format", ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
 
