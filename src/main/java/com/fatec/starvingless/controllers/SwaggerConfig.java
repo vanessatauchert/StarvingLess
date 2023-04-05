3788965d4ad76f0eedbe23aae8b0d3248367b6d6
Vanessa <tauchertvanessa@gmail.com>
2023-04-05 15:34:09 -0300

    add Athentication for SwaggerV3

--- src/main/java/com/fatec/starvingless/controllers/SwaggerConfig.java
+++ src/main/java/com/fatec/starvingless/controllers/SwaggerConfig.java
@@ -7,11 +7,13 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -20,8 +22,10 @@ public class SwaggerConfig  {
 
     @Bean
     public OpenAPI customApi() {
+        final String securitySchemeName = "basicAuth";
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("bearer-key", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
                 .info(new Info()
                         .title("API Starving Less")
                         .description("Projeto de TCC")
@@ -30,9 +34,14 @@ public class SwaggerConfig  {
                                 .name("Rafael Medeiros and Vanessa Oliveira")
                                 .url("url do front"))
 
-                )
+
+                ).addSecurityItem(new SecurityRequirement()
+                        .addList("bearer-jwt", Arrays.asList("reader", "write"))
+                        .addList("bearer-key", Collections.emptyList()))
                 ;
     }
+
+
 
 
 
